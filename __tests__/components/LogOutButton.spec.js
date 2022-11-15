bd4b5e507df218870d731a7121487be586f4ef69
Shah.Dev <Shah.dev@onsolve.com>
2022-11-15 16:04:27 +0530

    removed comments

--- __tests__/components/LogOutButton.spec.js
+++ __tests__/components/LogOutButton.spec.js
@@ -42,31 +42,4 @@ describe("LogOutButton", () => {
     component.find('Button[testID="logout-btn"]').prop("onPress")();
     expect(component).toMatchSnapshot();
   });
-
-  // it("Btn onPress", () => {
-  //   const component = getComponent();
-
-  //   const btn = component.props();
-
-  //   console.log(btn);
-
-  //   expect(component).toMatchSnapshot();
-  // });
 });
-// it("button on press", () => {
-//   const component = getComponent();
-//   const { getByTestId } = render(<LogOutButton />);
-//   const button = getByTestId("logout-btn");
-//   fireEvent.press(button);
-//   expect(auth.currentUser).toBe(null);
-//   expect(component).toMatchSnapshot();
-// });
-
-// it("button on press fail", () => {
-//   const component = getComponent();
-//   const { getByTestId } = render(<LogOutButton />);
-//   const button = getByTestId("logout-btn");
-//   fireEvent.press(button);
-//   auth.currentUser = User();
-//   expect(auth.currentUser).not.toBeNull();
-// });
